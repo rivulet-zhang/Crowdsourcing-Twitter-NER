8a32a205c94cad75eff739f20e1af49806782330
Jiawei Zhang <zhan1486@purdue.edu>
2017-04-16 17:35:38 -0400

    Adjust styling;

--- static/constant.js
+++ static/constant.js
@@ -2,3 +2,7 @@
 
 // const ENTITY_COLOR = {'org':'#66c2a5', 'psn':'#fc8d62', 'loc':'#8da0cb'};
 const ENTITY_COLOR = {'org':'#e41a1c', 'psn':'#377eb8', 'loc':'#4daf4a'};
+
+function get_entity_color(type){
+	return ENTITY_COLOR[type] || undefined;
+}
