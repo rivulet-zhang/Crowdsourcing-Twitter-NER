03f3c94a01b909e3d6eb8a9db81e176fc3b458d7
Jiawei Zhang <zhan1486@purdue.edu>
2017-04-17 00:26:36 -0400

    Add utility functions;

--- static/main.js
+++ static/main.js
@@ -2,12 +2,12 @@
 
 // tweet['entity'] = []
 // tweet['entity'].append({'type':'org', 'term':'Purdue Univeristy', 'isAuto':True})
-function styling_tweet(tweet){
+function styling_tweet_entity(tweet){
 
 	var text_html = tweet['text'];
 
 	tweet['entity'].forEach(function(entity){
-		var s = text_html.search(entity.term);
+		var s = text_html.toLowerCase().search(entity.term.toLowerCase());
 		if(s < 0 || s >= text_html.length)
 			console.error("error parsing entity");
 		else{
@@ -18,4 +18,36 @@ function styling_tweet(tweet){
 
   tweet['text_tagged'] = text_html;
 	return tweet;
+}
+
+function get_tweet_entity(tweet){
+
+  return tweet['entity'].map(function(entity){
+                              return {term:entity.term, 'type':entity.type};
+                            });
+}
+
+function add_entity_in_tweets(tweets, term, type, comment){
+
+  tweets.forEach(function(tweet){
+
+    if(tweet['text'].toLowerCase().indexOf(term.toLowerCase()) !== -1){
+      tweet.entity.push({'type':type, 'term':term, 'isAuto':false, 'comment':comment});
+    }
+
+  });
+
+  return tweets;
+
+}
+
+function check_term_exist_in_tweets(tweets, term){
+
+    var flag = false;
+    tweets.forEach(function(tweet){
+      if(tweet['text'].toLowerCase().indexOf(term.toLowerCase()) !== -1){
+        flag = true;
+      }
+    });
+    return flag;
 }
